c52c8350f8ee4290761a227def39aa55f7f5a873
Diplow <u.boillot@gmail.com>
2019-02-14 18:31:46 +0100

    boilerplate

---
+++ front/app/containers/HomePage/HomePage.js
@@ -0,0 +1,43 @@
+/*
+ * HomePage
+ *
+ * This is the first thing users see of our App, at the '/' route
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
+import './style.scss';
+
+export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  /**
+   * when initial state username is not null, submit the form to load repos
+   */
+  componentDidMount() {
+    const { username, onSubmitForm } = this.props;
+    if (username && username.trim().length > 0) {
+      onSubmitForm();
+    }
+  }
+
+  render() {
+    const {
+      loading, error, username, onChangeUsername, onSubmitForm
+    } = this.props;
+
+    return (
+      <div>
+        <Helmet><title>Vue densemble</title></Helmet>
+        <div className="home-page"></div>
+      </div>
+    );
+  }
+}
+
+HomePage.propTypes = {
+  loading: PropTypes.bool,
+  error: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+  onSubmitForm: PropTypes.func,
+  username: PropTypes.string,
+  onChangeUsername: PropTypes.func
+};
