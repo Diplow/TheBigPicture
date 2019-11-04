11d75c3f08d301b91b1d8b3f49885cfb0b7284fd
Ubuntu <ubuntu@ip-172-31-43-237.eu-west-3.compute.internal>
2019-11-04 15:06:12 +0000

    prod

--- back/api/settings.py
+++ back/api/settings.py
@@ -115,7 +115,7 @@ DATABASES = {
         'NAME': 'vuedensemble',
         'USER': 'diplo',
         'PASSWORD': 'vuedensemble',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '',
     }
 }
@@ -164,7 +164,22 @@ USE_S3 = os.getenv('USE_S3') == 'TRUE'
 
 if not USE_S3:
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+else:
+    # aws settings
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_DEFAULT_ACL = None
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    STATIC_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+    STATICFILES_STORAGE = 'api.storage_backends.StaticStorage'
+    #s3 public media settings
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'api.storage_backends.PublicMediaStorage'
 
 if DEPLOYEMENT_MODE == "prod":
     from api.prodsettings import *
