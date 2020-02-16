f3aff8be9961e93bad8d0827c9412df1b9af6848
Diplow <u.boillot@gmail.com>
2020-02-16 20:13:33 +0100

    gogo

--- back/api/settings.py
+++ back/api/settings.py
@@ -121,7 +121,7 @@ DATABASES = {
         'NAME': 'vuedensemble',
         'USER': 'diplo',
         'PASSWORD': 'vuedensemble',
-        'HOST': 'db',
+        'HOST': 'localhost', # db
         'PORT': '',
     }
 }
@@ -178,15 +178,15 @@ else:
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
     AWS_DEFAULT_ACL = None
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = '{bucket}.s3.amazonaws.com'.format(bucket=AWS_STORAGE_BUCKET_NAME)
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     # s3 static settings
     STATIC_LOCATION = 'static'
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+    STATIC_URL = 'https://{domain}/{location}/'.format(domain=AWS_S3_CUSTOM_DOMAIN, location=STATIC_LOCATION)
     STATICFILES_STORAGE = 'api.storage_backends.StaticStorage'
     #s3 public media settings
     PUBLIC_MEDIA_LOCATION = 'media'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    MEDIA_URL = 'https://{domain}/{location}/'.format(domain=AWS_S3_CUSTOM_DOMAIN, location=PUBLIC_MEDIA_LOCATION)
     DEFAULT_FILE_STORAGE = 'api.storage_backends.PublicMediaStorage'
 
 if DEPLOYEMENT_MODE == "prod":
