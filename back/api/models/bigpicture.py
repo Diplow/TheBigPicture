2285a5f9053f40ce635ac6f1e8a8153e2d2b9ebf
Diplow <u.boillot@gmail.com>
2020-06-27 11:40:05 +0200

    pin subjects (only via direct edition in the db)

--- back/api/models/bigpicture.py
+++ back/api/models/bigpicture.py
@@ -10,30 +10,33 @@ RESOURCE_CODE = 4
 
 
 class BigPicture(models.Model):
-	title = models.CharField(max_length=380)
-	body = models.TextField(blank=True)
-	kind = models.IntegerField(choices=(
-		("subject", SUBJECT_CODE),
-		("problem", PROBLEM_CODE),
-		("solution", SOLUTION_CODE),
-		("resource", RESOURCE_CODE),
-	))
-	private = models.BooleanField()
-	hyperlink = models.ForeignKey("self", blank=True, null=True, on_delete=models.CASCADE, related_name="references")
-	parent = models.ForeignKey("self", blank=True, null=True, on_delete=models.CASCADE, related_name="allchildren")
-	subject = models.ForeignKey("self", blank=True, null=True, on_delete=models.CASCADE, related_name="allfamily")
-	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	creation_date = models.DateField(auto_now_add=True)
-	modification_date = models.DateTimeField(auto_now=True)
+  title = models.CharField(max_length=380)
+  body = models.TextField(blank=True)
+  kind = models.IntegerField(choices=(
+    ("subject", SUBJECT_CODE),
+    ("problem", PROBLEM_CODE),
+    ("solution", SOLUTION_CODE),
+    ("resource", RESOURCE_CODE),
+  ))
+  pin = models.BooleanField()
+  private = models.BooleanField()
+  hyperlink = models.ForeignKey("self", blank=True, null=True, on_delete=models.CASCADE, related_name="references")
+  parent = models.ForeignKey("self", blank=True, null=True, on_delete=models.CASCADE, related_name="allchildren")
+  subject = models.ForeignKey("self", blank=True, null=True, on_delete=models.CASCADE, related_name="allfamily")
+  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  creation_date = models.DateField(auto_now_add=True)
+  modification_date = models.DateTimeField(auto_now=True)
 
-	def save(self, *args, **kwargs):
-		super(BigPicture, self).save(*args, **kwargs)
-		if self.subject is None:
-			self.subject = self
-			self.save()
+  def save(self, *args, **kwargs):
+    if self.pin is None:
+      self.pin = False
+    super(BigPicture, self).save(*args, **kwargs)
+    if self.subject is None:
+      self.subject = self
+      self.save()
 
-	def __unicode__(self):
-		return self.id
+  def __unicode__(self):
+    return self.id
 
-	def __str__(self):
-		return "[{kind}] - {title} - {id}".format(kind=self.kind, title=self.title, id=self.id)
+  def __str__(self):
+    return "[{kind}] - {title} - {id}".format(kind=self.kind, title=self.title, id=self.id)
