f8f7db6da640a57eb18b374cf827c6daa7691da5
Max Dav <maxdav@localhost.localdomain>
2020-08-05 13:06:12 +0200

    Add local docker-compose

---
+++ back/api/localsettings.py
@@ -0,0 +1,12 @@
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('SQL_DATABASE', 'postgres'),
+        'USER': os.getenv('SQL_USER', 'postgres'),
+        'PASSWORD': os.getenv('SQL_PASSWORD', 'postgres'),
+        'HOST': os.getenv('SQL_HOST', 'db'),
+        'PORT': '5432',
+    }
+}
--- back/api/settings.py
+++ back/api/settings.py
@@ -14,7 +14,7 @@ import os
 import datetime
 
 DEPLOYEMENT_MODE = os.getenv("DEPLOYEMENT_MODE")
-
+print(DEPLOYEMENT_MODE)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname((os.path.abspath(__file__))))
@@ -192,3 +192,6 @@ else:
 
 if DEPLOYEMENT_MODE == "prod":
     from api.prodsettings import *
+
+if DEPLOYEMENT_MODE == "local":
+    from api.localsettings import *
