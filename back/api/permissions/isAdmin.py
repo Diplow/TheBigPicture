d5781278d9a3af480ec3aa952917e22822ea72a9
Diplow <u.boillot@gmail.com>
2020-08-29 09:52:03 +0200

    hotfix

--- back/api/permissions/isAdmin.py
+++ back/api/permissions/isAdmin.py
@@ -6,3 +6,8 @@ class IsAdmin(permissions.BasePermission):
 
   def has_permission(self, request, view):
     return request.user and request.user.is_staff
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS or (request.user and request.user.is_staff)
